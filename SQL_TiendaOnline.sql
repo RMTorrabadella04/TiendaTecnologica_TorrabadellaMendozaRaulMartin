43e41f4728802071474afce37fff0d2fedf2aed8
RMTorrabadella04 <raul.martin.torrabadella@gmail.com>
2024-11-26 19:52:54 +0100

    Version 1.0

---
+++ SQL_TiendaOnline.sql
@@ -0,0 +1,48 @@
+CREATE DATABASE tiendaonline;
+
+USE tiendaonline;
+
+-- Tablas de los Productos y sus Caracteristicas
+
+CREATE TABLE Producto (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(20) NOT NULL,
+    descripcion VARCHAR(100) NOT NULL,
+    precio DECIMAL(10, 2) NOT NULL,
+    inventario INT NOT NULL,
+    categoria_nombre VARCHAR(255) NOT NULL, 
+    url VARCHAR(255)
+);
+
+
+CREATE TABLE Caracteristicas (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    producto_id INT NOT NULL,
+    nombre VARCHAR(20) NOT NULL,
+    valor VARCHAR(50) NOT NULL,
+    FOREIGN KEY (producto_id) REFERENCES Producto(id)
+);
+
+
+-- Tablas de los Usuarios y su Historial de Compra
+
+CREATE TABLE Usuario (
+    id INT PRIMARY KEY,
+    nombre VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    calle VARCHAR(255) NOT NULL,
+    numero INT NOT NULL,
+    ciudad VARCHAR(255) NOT NULL,
+    pais VARCHAR(255) NOT NULL
+);
+
+
+CREATE TABLE HistorialCompra (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    usuario_id INT NOT NULL,
+    producto_id INT NOT NULL,
+    cantidad INT NOT NULL,
+    fecha DATE NOT NULL,
+    FOREIGN KEY (usuario_id) REFERENCES Usuario(id),
+    FOREIGN KEY (producto_id) REFERENCES Producto(id)
+);
---
+++ src/main/java/com/mycompany/tiendaonline/ConexionBBDDJson.java
@@ -0,0 +1,320 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.tiendaonline;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.SimpleDateFormat;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+
+
+
+/**
+ *
+ * @author Raul
+ */
+
+public class ConexionBBDDJson {
+    
+    private String url;
+    private String usuario;
+    private String contrasenya; 
+    private Connection conexion;
+    
+    // Constructor
+    
+    public ConexionBBDDJson()  throws FileNotFoundException, ParseException, IOException, SQLException{
+        
+        url = "jdbc:mysql://localhost:3306/tiendaonline";
+        usuario = "root";
+        contrasenya = "";
+        
+         try {
+  
+            conexion = DriverManager.getConnection(url, usuario, contrasenya);
+            
+            System.out.println("Conexión exitosa a la base de datos!");
+            
+        } catch (SQLException e) {
+            
+            System.err.println("Error al conectar con la base de datos: " + e.getMessage());
+            
+        }
+         
+        String checkUserQuery = "SELECT COUNT(*) FROM Usuario";
+        
+        try (PreparedStatement stmtCheckUser = conexion.prepareStatement(checkUserQuery)) {
+            
+            ResultSet rs = stmtCheckUser.executeQuery();
+            
+            if (rs.next() && rs.getInt(1) > 0) {
+                
+                // Si ya existen registros, no se ejecuta conectarYLeerJson()
+                
+                System.out.println("Datos ya existen en la base de datos, no es necesario insertar.");
+                
+            } else {
+                
+                // Si no existen datos, procede a insertar los datos del JSON
+                
+                conectarYLeerJson();
+                
+            }
+        }
+         
+                 
+        
+    }
+    
+    // Clases de JSON y listas que contienen los datos que luego pasamos a la BBDD
+    
+    private void conectarYLeerJson() throws FileNotFoundException, ParseException, IOException, SQLException{
+        
+        JSONParser parser = new JSONParser();
+        
+        FileReader reader = new FileReader("src/main/Resources/tiendaOnline.json");
+        
+        Object obj = parser.parse(reader);
+        
+        JSONObject tienda = new JSONObject ((Map) obj);
+        
+        procesarProductos(tienda);
+        
+        procesarUsuarios(tienda);
+        
+    }
+    
+    // Recorre los el JSON con for each para sacar los productos que después seran añadidos a la base de datos
+    
+    private void procesarProductos(JSONObject obj) throws SQLException{
+        
+        JSONObject tiendecita = (JSONObject) obj.get("tienda");
+        
+        JSONArray arrayCategorias = (JSONArray) tiendecita.get("categorias");
+        
+        // Para recorrer todas las categorias
+        
+        for (Object cate : arrayCategorias){
+            
+            JSONObject categorias = (JSONObject) cate;
+            
+            JSONArray arrayProductos = (JSONArray) categorias.get("productos");
+            
+            String nombre = (String) categorias.get("nombre");
+                       
+            // Para recorrer cada producto dentro de una categoria concreta
+            
+            for (Object pro : arrayProductos){
+                
+                JSONObject productos = (JSONObject) pro;
+                
+                JSONObject caracteristicas = (JSONObject) productos.get("caracteristicas");
+                
+                // Hacemos el insert de los datos productos del JSON a la tabla correspondiente de MySQL
+                
+                int idProducto = ((Long) productos.get("id")).intValue();
+                
+                String nombreProducto = (String) productos.get("nombre");
+                
+                String descripcion = (String) productos.get("descripcion");
+                
+                double precio = (Double) productos.get("precio");
+                
+                int inventario = ((Long) productos.get("inventario")).intValue();
+                
+                String url = (String) productos.get("url");
+
+                // Para hacer el insert en la BBDD
+                
+                String sql = "INSERT INTO Producto (id, nombre, descripcion, precio, inventario, categoria_nombre, url) VALUES (?, ?, ?, ?, ?, ?, ?)";
+               
+                try (PreparedStatement stmt = conexion.prepareStatement(sql)) {
+                    
+                    stmt.setInt(1, idProducto);
+                    
+                    stmt.setString(2, nombreProducto);
+                    
+                    stmt.setString(3, descripcion);
+                    
+                    stmt.setDouble(4, precio);
+                    
+                    stmt.setInt(5, inventario);
+                    
+                    stmt.setString(6, nombre);
+                    
+                    stmt.setString(7, url);
+
+                    stmt.executeUpdate();
+                    
+                    System.out.println("Producto insertado correctamente.");
+                    
+                } catch (SQLException e) {
+                    
+                    System.err.println("Error al insertar el producto: " + e.getMessage());
+                    
+                }
+                
+                // Recorre cada caracteristica del producto
+                
+                for (Object carac : caracteristicas.keySet()){
+                    
+                    String nombreCaracteristica = (String) carac;
+                    
+                    String valor = (String) caracteristicas.get(nombreCaracteristica);
+                    
+                    String sqlCaracteristica = "INSERT INTO Caracteristicas (producto_id, nombre, valor) VALUES (?, ?, ?)";
+                    
+                    // Y lo guarda en la BBDD
+        
+                    try (PreparedStatement stmt = conexion.prepareStatement(sqlCaracteristica)) {
+                        
+                        stmt.setInt(1, idProducto);
+                        
+                        stmt.setString(2, nombreCaracteristica);
+                        
+                        stmt.setString(3, valor);
+                        
+                        stmt.executeUpdate();
+                        
+                        System.out.println("Característica '" + nombreCaracteristica + "' insertada correctamente.");
+                        
+                    } catch (SQLException e) {
+                        
+                        System.err.println("Error al insertar la característica: " + e.getMessage());
+                        
+                    }
+                }
+            }
+        }
+    }
+    
+    
+   private void procesarUsuarios(JSONObject obj) throws SQLException{
+       
+       JSONObject tiendecita = (JSONObject) obj.get("tienda");
+       
+       JSONArray arrayUsuarios = (JSONArray) tiendecita.get("usuarios");
+       
+       // Para recorrer los usuarios
+       
+       for (Object usu: arrayUsuarios){
+        
+           JSONObject usuario = (JSONObject) usu;
+           
+           int idUsuario = ((Long) usuario.get("id")).intValue();
+            
+           String nombre= (String) usuario.get("nombre");
+            
+           String email= (String) usuario.get("email");
+            
+           JSONObject direccion= (JSONObject) usuario.get("direccion");
+           
+           Object numero = direccion.get("numero");
+
+           // Lo convertimos a String ya que asi está en la base de datos
+           String numeroDireccion = String.valueOf(numero);
+            
+           String sqlUsuario = "INSERT INTO Usuario (id, nombre, email, calle, numero, ciudad, pais) VALUES (?, ?, ?, ?, ?, ?, ?)";
+           
+            
+           // Para insertarlos en la BBDD
+            
+           try (PreparedStatement stmt = conexion.prepareStatement(sqlUsuario)) {
+                    
+               stmt.setInt(1, idUsuario);
+                    
+               stmt.setString(2, nombre);
+                    
+               stmt.setString(3, email);
+                    
+               stmt.setString(4, (String) direccion.get("calle"));
+                    
+               stmt.setString(5, numeroDireccion);
+                    
+               stmt.setString(6, (String) direccion.get("ciudad"));
+                    
+               stmt.setString(7, (String) direccion.get("pais"));
+
+               stmt.executeUpdate();
+                    
+               System.out.println("Usuario insertado correctamente.");
+                    
+           } catch (SQLException e) {
+                    
+               System.err.println("Error al insertar el producto: " + e.getMessage());
+                    
+           } 
+           
+           JSONArray arrayHistorialCompra = (JSONArray) usuario.get("historialCompras");
+           
+           // Para recorrer el Historial de Compra
+           
+           for (Object hc: arrayHistorialCompra){
+               
+               JSONObject historial = (JSONObject) hc;
+               
+               int idProducto = ((Long) historial.get("productoId")).intValue();
+               
+               int idCantidad = ((Long) historial.get("cantidad")).intValue();
+               
+               String fecha= (String) historial.get("fecha");
+               
+                java.sql.Date fechaSql = null;
+                
+               try {
+                   
+                    SimpleDateFormat formato = new SimpleDateFormat("yyyy-MM-dd");
+                   
+                    java.util.Date fechaUtil = formato.parse(fecha);
+                    
+                    fechaSql = new java.sql.Date(fechaUtil.getTime());
+                    
+                   
+               } catch (java.text.ParseException ex) {
+                  
+                    Logger.getLogger(ConexionBBDDJson.class.getName()).log(Level.SEVERE, null, ex);
+                   
+               }
+               
+               String sqlHistorial = "INSERT INTO HistorialCompra (usuario_id, producto_id, cantidad, fecha) VALUES (?, ?, ?, ?)";
+               
+               // Para insertarlo en la BBDD
+
+               try (PreparedStatement stmt = conexion.prepareStatement(sqlHistorial)) {
+                    
+                    stmt.setInt(1, idUsuario);
+
+                    stmt.setInt(2, idProducto);
+
+                    stmt.setInt(3, idCantidad);
+
+                    stmt.setDate(4,fechaSql);
+
+                    stmt.executeUpdate();
+
+                    System.out.println("Historial de Compra insertado correctamente.");
+
+                } catch (SQLException e) {
+
+                    System.err.println("Error al insertar el producto: " + e.getMessage());
+
+                }
+           }
+       }
+   } 
+}
+
